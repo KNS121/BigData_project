1c3544e65b9e8453153f633b6a64fa6f734cbe07
KNS_121 <komaroff.ns@gmail.com>
2024-11-18 01:07:34 +0300

    Попытка 2 сделать таблички в БД с периодами работы каждой скважины

--- Wells_time_period_work/WellActiveTime.py
+++ Wells_time_period_work/WellActiveTime.py
@@ -35,7 +35,60 @@ class WellActivityPeriodsManager:
         self.db_connector.commit()
         print("Таблицы oil_activity_periods и ppd_activity_periods созданы.")
 
+    def create_indexes(self, table_name):
+        create_index_well = f"""
+        CREATE INDEX IF NOT EXISTS idx_{table_name}_well ON {table_name} ("Скважина");
+        """
+
+        create_index_field = f"""
+        CREATE INDEX IF NOT EXISTS idx_{table_name}_field ON {table_name} ("Месторождение");
+        """
+
+        create_index_date = f"""
+        CREATE INDEX IF NOT EXISTS idx_{table_name}_date ON {table_name} ("name_of_date_doc_col");
+        """
+
+        self.db_connector.execute_query(create_index_well, fetch_results=False)
+        self.db_connector.execute_query(create_index_field, fetch_results=False)
+        self.db_connector.execute_query(create_index_date, fetch_results=False)
+        self.db_connector.commit()
+        print(f"Индексы созданы для таблицы {table_name}.")
+
+    def analyze_query(self, query):
+        explain_query = f"EXPLAIN ANALYZE {query}"
+        self.db_connector.execute_query(explain_query, fetch_results=True)
+        results = self.db_connector.cur.fetchall()
+        for row in results:
+            print(row)
+        
+        
+        
     def insert_data(self, table_name, well_type_table, value_column):
+        
+        create_temp_table_query = f"""
+        CREATE TEMP TABLE temp_active_periods AS
+        SELECT
+            "Скважина"::TEXT,
+            "Месторождение"::INTEGER,
+            TO_DATE("name_of_date_doc_col", 'YYYY-MM-DD') AS date,
+            LAG(TO_DATE("name_of_date_doc_col", 'YYYY-MM-DD')) OVER (PARTITION BY "Скважина" ORDER BY TO_DATE("name_of_date_doc_col", 'YYYY-MM-DD')) AS prev_date,
+            LEAD(TO_DATE("name_of_date_doc_col", 'YYYY-MM-DD')) OVER (PARTITION BY "Скважина" ORDER BY TO_DATE("name_of_date_doc_col", 'YYYY-MM-DD')) AS next_date,
+            LAG("{value_column}") OVER (PARTITION BY "Скважина" ORDER BY TO_DATE("name_of_date_doc_col", 'YYYY-MM-DD')) AS prev_value1,
+            LEAD("{value_column}") OVER (PARTITION BY "Скважина" ORDER BY TO_DATE("name_of_date_doc_col", 'YYYY-MM-DD')) AS next_value1
+        FROM
+            {well_type_table}
+        WHERE
+            "{value_column}" IS NOT NULL;
+        """
+
+        self.db_connector.execute_query(create_temp_table_query, fetch_results=False)
+        self.db_connector.commit()
+        
+        
+        
+        
+        
+        
         insert_data_query = f"""
         WITH RECURSIVE active_periods AS (
             SELECT
@@ -112,6 +165,9 @@ class WellActivityPeriodsManager:
             "Скважина",
             start_date;
         """
+        
+        self.analyze_query(insert_data_query)
+        
         self.db_connector.execute_query(insert_data_query, fetch_results=False)
         self.db_connector.commit()
         print(f"Данные вставлены в таблицу {table_name}.")
@@ -129,6 +185,9 @@ def create_active_table():
 
     well_activity_manager.create_tables()
 
+    well_activity_manager.create_indexes('df_oil_for_bd')
+    well_activity_manager.create_indexes('df_ppd_for_bd')
+    
     well_activity_manager.insert_data('oil_activity_periods', 'df_oil_for_bd', 'Факт_дебит_жидкостим3_сут')
     well_activity_manager.insert_data('ppd_activity_periods', 'df_ppd_for_bd', 'Факт_приемистость_м3_сут')
 
