cd6ccc590fb924d97f8906632feb56477bf5c59d
KNS121 <102309501+KNS121@users.noreply.github.com>
2024-12-14 21:27:57 +0300

    Delete BigDataApplication directory

--- BigDataApplication/application/Task2/create_result_table_time_analys.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS avgtime (
-            date_origin VARCHAR(7),
-            field INTEGER,
-            avg_time_prod FLOAT,
-            avg_time_inj FLOAT
-        );
-SELECT create_distributed_table('avgtime', 'field');
--- BigDataApplication/application/common/Read_SQL.py
+++
@@ -1,3 +0,0 @@
-def read_sql_file(file_path, encoding='utf-8'):
-    with open(file_path, 'r', encoding=encoding) as file:
-        return file.read()
